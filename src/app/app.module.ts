9e461b206a191d050f03706c4a8c12e7e95738d8
Fábio da S. Jordão <fjordao@dcc.local>
2019-07-24 14:22:20 -0300

    Separando os component do layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavtopComponent } from './navtop/navtop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
+    NavtopComponent,
   ],
   imports: [
     BrowserModule,
