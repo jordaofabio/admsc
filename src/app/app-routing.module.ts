67d9001d03f733436e144d9c26fc03b802b9784c
jordaofabio <fabiosjordao@gmail.com>
2019-08-05 01:24:03 -0300

    Base do módulo pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ const routes: Routes = [
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule'},
   { path: 'users', loadChildren: './users/users.module#UsersModule'},
   { path: 'users/new', loadChildren: './users/users.module#UsersModule'},
+  { path: 'users/edit/:idUser', loadChildren: './users/users.module#UsersModule'},
+  { path: 'pages', loadChildren: './pages/pages.module#PagesModule'},
 ];
 
 @NgModule({
