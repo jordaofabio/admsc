7d7f337e0940cf61d30964478d68a174a6ca94b5
jordaofabio <fabiosjordao@gmail.com>
2019-09-17 02:11:42 -0300

    Ajustes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 3000
+const app = express()
+ 
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'dist', 'admsc'
+)))
+ 
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'dist', 'admsc', 'index.html'))
+})
+
+app.get('/api/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'dist', 'api', 'index.html'))
+})
+ 
+app.listen(port)
